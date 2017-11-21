262bfbac112d56d2fde0bc5c00ba300c02401bf8
Mathis AUBRY <mathis.aubry@ynov.com>
2017-11-21 14:55:15 +0100

    Exercise 1

---
+++ test/shop-tools.test.js
@@ -0,0 +1,13 @@
+function getPrices()
+{
+    var shop = {
+        banana: 1,
+        potato: 2,
+        tomato: 3,
+        cucumber: 4,
+        salad: 5,
+        apple: 6
+    }
+    return (shop);
+}
+console.log(getPrices());
